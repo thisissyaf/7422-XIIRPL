1588ef68ef14da3f27cf327bc911416db1ad3d11
thisissyaf <syaf.faa32@gmail.com>
2023-07-26 08:14:31 +0700

    fisrt github project

---
+++ app.py
@@ -0,0 +1,7 @@
+def luas_segitiga():
+    a = int(input("masukkan alas segitiga: "))
+    t = int(input("masukkan tinggi segitiga: "))
+    luas = a * t / 2
+    print("luas segitiga adalah: ", luas)
+
+luas_segitiga()
